e0bcf537b0d7f81cef6e4ef9c7e97587fe52c5f1
edidiesky <essienedidiong1000@gmail.com>
2023-10-16 17:45:03 -0700

    new

--- frontend/src/components/home/banner.tsx
+++ frontend/src/components/home/banner.tsx
@@ -1,11 +1,71 @@
 import styled from "styled-components";
 import { BannerStyles } from "./styles/banner";
+import Image from "../common/Image";
 
-
+const BannerData = [
+    {
+        height: "100%",
+        width: '100%',
+        src: "https://s.udemycdn.com/partner-logos/ou-v1/volkswagen.svg"
+    },
+    {
+        height: "100%",
+        width: '100%',
+        src: "https://s.udemycdn.com/partner-logos/ou-v1/samsung.svg"
+    },
+    {
+        height: "100%",
+        width: '100%',
+        src: "https://s.udemycdn.com/partner-logos/ou-v1/att.svg"
+    },
+    {
+        height: "100%",
+        width: '100%',
+        src: "https://s.udemycdn.com/partner-logos/ou-v1/cisco.svg"
+    },
+    {
+        height: "100%",
+        width: '100%',
+        src: "https://s.udemycdn.com/partner-logos/ou-v1/procter_gamble.svg"
+    },
+    {
+        height: "100%",
+        width: '100%',
+        src: "https://s.udemycdn.com/partner-logos/ou-v1/hewlett_packard_enterprise.svg"
+    },
+    {
+        height: "100%",
+        width: '100%',
+        src: "https://s.udemycdn.com/partner-logos/ou-v1/citi.svg"
+    },
+    {
+        height: "100%",
+        width: '100%',
+        src: 'https://s.udemycdn.com/partner-logos/ou-v1/ericsson.svg'
+    }
+]
 const Banner = () => {
     return (
         <BannerStyles>
+            <div className="banner_wrapper background w-100">
+                <div className="w-85 auto flex column gap-3">
+                    <h4 className="fs-24 text-grey2 text-light text-center">
+                        Trusted by over 14,400 companies and millions of learners around the world
+                    </h4>
+                    <div className="w-100 support grid gap-2">
+                        {
+                            BannerData.map((data?: any, index?: any) => {
+                                return <div style={{height:"5rem",width:"100%"}} className="w-100">
+                                    <Image
 
+                                        {...data}
+                                    />
+                                </div>
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
         </BannerStyles>
     )
 }
--- frontend/src/components/home/styles/banner.tsx
+++ frontend/src/components/home/styles/banner.tsx
@@ -1,3 +1,14 @@
 import styled from "styled-components";
 
-export const BannerStyles = styled.div``;
+export const BannerStyles = styled.div`
+  width: 100%;
+  margin: 5rem 0;
+  .banner_wrapper{
+    padding: 6rem 0;
+  }
+  .support {
+    display: grid;
+    grid-gap: 5rem;
+    grid-template-columns: repeat(auto-fit,minmax(100px,1fr));
+  }
+`;
